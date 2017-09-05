fa72ed8be58d2b6272cbbdcef761d2492d0e02e2
David Anglin <davidanglin@davids-mbp.lan>
2017-09-05 13:59:10 -0500

    Set up initial models

---
+++ iTunesSearchClient/Artist.swift
@@ -0,0 +1,25 @@
+//
+//  Artist.swift
+//  iTunesSearchClient
+//
+//  Created by David Anglin on 9/5/17.
+//  Copyright © 2017 David Anglin. All rights reserved.
+//
+
+import Foundation
+
+class Artist {
+    let id: Int
+    let name: String
+    let primaryGenre: Genre
+    var albums: [Album]
+    
+    init(id: Int, name: String, primaryGenre: Genre, albums: [Album]) {
+        self.id = id
+        self.name = name
+        self.primaryGenre = primaryGenre
+        self.albums = albums
+    }
+    
+    
+}
---
+++ iTunesSearchClient/Song.swift
@@ -0,0 +1,18 @@
+//
+//  Song.swift
+//  iTunesSearchClient
+//
+//  Created by David Anglin on 9/5/17.
+//  Copyright © 2017 David Anglin. All rights reserved.
+//
+
+import Foundation
+
+struct Song {
+    
+    let id: Int
+    let name: String
+    let censoredName: String
+    let trackTime: Int
+    let isExplicit: Bool
+}
